17b45f2a85897d4d43a444d14569ea3409c51af8
seb3point0 <seb3point0@mba.home>
2023-01-06 21:32:59 +0100

    add puppeteer flags

--- src/api/create-pdf.js
+++ src/api/create-pdf.js
@@ -13,7 +13,7 @@ router.post('/', async (req, res) => {
       left : "4cm"
     }
   }
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions', '--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.setContent(html);
   await page.emulateMediaType('screen');
